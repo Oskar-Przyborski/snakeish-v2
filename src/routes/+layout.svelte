4c0e9337865ea9f1d3cf465cbec219a7729bb52f
Oskar-Przyborski <oskar.przski@gmail.com>
2022-12-31 18:39:01 +0100

    moved footer and container to layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Container from '$lib/components/container.svelte';
+	import Footer from '$lib/components/footer.svelte';
+</script>
+<Container>
+    <slot />
+</Container>
+<Footer />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,21 @@
 <script lang="ts">
 	import type { PageServerData } from './$types';
-	import Container from '$lib/components/container.svelte';
 	import AvailableRooms from './available_rooms.svelte';
 	import GameDescription from './game_description.svelte';
-	import Footer from '$lib/components/footer.svelte';
 
 	export let data: PageServerData;
 	const rooms = data.data ? data.data : [];
 </script>
 
-<Container>
-	<div class="snakeish-logo">
-		<img alt="Snakeish Logo" src="/logo-long-white.png" width="310" height="60" />
-	</div>
-	{#if data.isOnline}
-		<AvailableRooms {rooms} />
-	{:else}
-		Server is offline.
-	{/if}
-	<GameDescription />
-</Container>
-<Footer />
+<div class="snakeish-logo">
+	<img alt="Snakeish Logo" src="/logo-long-white.png" width="310" height="60" />
+</div>
+{#if data.isOnline}
+	<AvailableRooms {rooms} />
+{:else}
+	Server is offline.
+{/if}
+<GameDescription />
 
 <style>
 	.snakeish-logo {
