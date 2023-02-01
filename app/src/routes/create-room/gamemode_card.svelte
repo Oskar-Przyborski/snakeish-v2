8a0aa66b8a5f8f9cb110ca84618490bebc04e42e
Oskar-Przyborski <oskar.przski@gmail.com>
2023-02-01 17:55:14 +0100

    Add: checkmark on selected gamemode card

--- app/src/routes/create-room/gamemode_card.svelte
+++ app/src/routes/create-room/gamemode_card.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import modes from '$lib/modes';
+	import Icon from '@iconify/svelte';
 	import { createEventDispatcher } from 'svelte';
 	const dispatcher = createEventDispatcher();
 
@@ -9,15 +10,17 @@
 	$: mode = modes.get(configName)!;
 </script>
 
-<button
-	class="gamemode-card"
-	class:selected
-	on:click={() => dispatcher('select')}
-	on:keypress={() => dispatcher('select')}
->
-	<div class="name">
-		<h2>{mode.title}</h2>
-		<div class="tag">{mode.tag.toUpperCase()}</div>
+<button class="gamemode-card" class:selected on:click={() => dispatcher('select')}>
+	<div class="top">
+		<div class="name">
+			<h2>{mode.title}</h2>
+			<div class="tag">{mode.tag.toUpperCase()}</div>
+		</div>
+		{#if selected}
+			<div class="checkbox">
+				<Icon icon="mdi:check-bold" inline />
+			</div>
+		{/if}
 	</div>
 	<div class="description">{mode.description}</div>
 </button>
@@ -30,26 +33,42 @@
 		padding: 1rem;
 		transition: border 0.1s ease-in-out, box-shadow 0.1s ease-in-out;
 		cursor: pointer;
-
-		.name {
+		.top {
 			display: flex;
 			flex-flow: row nowrap;
-			gap: .5rem;
-			align-items: center;
-			.tag {
-				font-size: 0.75rem;
-				font-weight: bold;
-				background-color: #0000002b;
-				width: max-content;
-				padding: 0.3rem;
+			gap: 1rem;
+			justify-content: space-between;
+			align-items: flex-start;
+			.name {
+				display: flex;
+				flex-flow: row nowrap;
+				gap: 0.5rem;
+				align-items: center;
+				.tag {
+					font-size: 0.75rem;
+					font-weight: bold;
+					background-color: #0000002b;
+					width: max-content;
+					padding: 0.3rem;
+				}
+				h2 {
+					margin: 0.3rem 0;
+				}
 			}
-			h2 {
-				margin: 0.3rem 0;
+
+			.checkbox {
+				font-size: 1.2rem;
+				background-color: var(--red);
+				width: 1.6rem;
+				height: 1.6rem;
+				display: grid;
+				place-items: center;
+				border-radius: 0.3rem;
 			}
 		}
 
 		&:hover {
-			box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.384);
+			box-shadow: 0px 3px 7px rgba(0, 0, 0, 0.2);
 		}
 		&:focus-visible {
 			outline-color: rgb(255, 255, 255);
@@ -60,7 +79,7 @@
 
 		&.selected {
 			border: 3px solid #d62246;
-			box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.384);
+			// box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.384);
 		}
 	}
 </style>
