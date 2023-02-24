815c33ff6ca611014fbc9cf51719d6f2e349f9a2
Squar <oskar.przski@gmail.com>
2023-02-24 17:03:05 +0100

    removed nonexisted mode

--- app/src/lib/modes.ts
+++ app/src/lib/modes.ts
@@ -17,13 +17,4 @@ export default new Map<string, { tag: string; title: string; name: string; descr
 			description: 'Quite big map with many players and apples'
 		}
 	],
-	[
-		'battle-royale-eat-n-win',
-		{
-			tag: 'battle royale',
-			title: 'Eat & Win',
-			name: 'battle-royale-eat-n-win',
-			description: 'Popular battle royale mode, with one life and shrinking zones'
-		}
-	]
 ]);
