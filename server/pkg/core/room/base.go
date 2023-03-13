51576a2ad777732b6ac5f7fcb7cb3e973b50db31
Squar <oskar.przski@gmail.com>
2023-03-13 14:51:08 +0100

    extraceted pin requirer

--- server/pkg/core/room/base.go
+++ server/pkg/core/room/base.go
@@ -33,31 +33,3 @@ type RoomPreview struct {
 	MaxPlayers int    `json:"maxPlayers"`
 	PinEnbled  bool   `json:"pinEnabled"`
 }
-
-type PinRequirer struct {
-	PinEnabled bool
-	Pin        [4]int
-}
-
-func (manager PinRequirer) CheckPin(value [4]int) bool {
-	if manager.PinEnabled {
-		return manager.Pin == value
-	} else {
-		return true
-	}
-}
-
-func (manager PinRequirer) IsPinEnabled() bool {
-	return manager.PinEnabled
-}
-
-func CreatePinRequirer(pin *[4]int) PinRequirer {
-	pinRequirer := PinRequirer{}
-	if pin == nil {
-		pinRequirer.PinEnabled = false
-	} else {
-		pinRequirer.PinEnabled = true
-		pinRequirer.Pin = *pin
-	}
-	return pinRequirer
-}
---
+++ server/pkg/core/room/pin_requirer.go
@@ -0,0 +1,29 @@
+package room
+
+type PinRequirer struct {
+	PinEnabled bool
+	Pin        [4]int
+}
+
+func (manager PinRequirer) CheckPin(value [4]int) bool {
+	if manager.PinEnabled {
+		return manager.Pin == value
+	} else {
+		return true
+	}
+}
+
+func (manager PinRequirer) IsPinEnabled() bool {
+	return manager.PinEnabled
+}
+
+func CreatePinRequirer(pin *[4]int) PinRequirer {
+	pinRequirer := PinRequirer{}
+	if pin == nil {
+		pinRequirer.PinEnabled = false
+	} else {
+		pinRequirer.PinEnabled = true
+		pinRequirer.Pin = *pin
+	}
+	return pinRequirer
+}
